06a19bed45d55a6663f92be267932a325a19fe82
Atul Bisht <atulbisht26@gmail.com>
2019-03-05 18:21:57 +0530

    Updates

---
+++ precache-manifest.097c405617edec14f8d7567039671b2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d728742ac6d647561539",
+    "url": "/phone-directory-app/static/css/main.6b3a1a1c.chunk.css"
+  },
+  {
+    "revision": "d728742ac6d647561539",
+    "url": "/phone-directory-app/static/js/main.d728742a.chunk.js"
+  },
+  {
+    "revision": "d86c83917acb4be60b88",
+    "url": "/phone-directory-app/static/js/runtime~main.d86c8391.js"
+  },
+  {
+    "revision": "9982b409e759213ed96b",
+    "url": "/phone-directory-app/static/js/2.9982b409.chunk.js"
+  },
+  {
+    "revision": "6953b876d26cc01c8845f6976182714c",
+    "url": "/phone-directory-app/index.html"
+  }
+];
